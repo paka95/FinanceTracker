54e51b692480fe6d48d58944291f8846cec6939a
paka95 <patryk.pomierski@gmail.com>
2022-03-26 02:25:19 +0100

    added date filtering (to be improved)

--- website/views.py
+++ website/views.py
@@ -3,7 +3,8 @@ from flask_login import login_required, current_user
 from website.forms import ExpenseForm
 from website.models import Expense
 from website import db
-from datetime import datetime
+from datetime import datetime, date
+from sqlalchemy.sql import func, and_
 
 
 views = Blueprint("views", __name__)
@@ -13,17 +14,41 @@ views = Blueprint("views", __name__)
 @login_required
 def home():
     form = ExpenseForm()
-    expenses = Expense.query.filter_by(user_id = current_user.id).all()
-    # print(type(expenses[1].date_created))
+    start = datetime(year=2022, month=3, day=25)
+    end = datetime(year=2022, month=3, day=26)
+    # expenses = Expense.query.filter_by(date_created = '2022-03-25 21:11:57', user_id = current_user.id).all()
+    expenses = Expense.query.filter((Expense.date_created > start) & (Expense.date_created < end)).filter_by(user_id = 1).all()
+    # expenses = Expense.query.filter_by(user_id = current_user.id).all()
+    # datah = request.form.get("datah") # 2022-03-25
+    terazczas = datetime.today()
+    terazdata = date.today()
+    print("type of expense: ", type(expenses[0].date_created))
+    print("date created z db", expenses[0].date_created)
+    print()
+    print("type of terazczas: ", type(terazczas))
+    print("terazczas", terazczas)
+    print("terazczas", terazczas.strftime('%Y-%m-%d %H:%M:%S'))
+    print()
+    print("typ terazdata", type(terazdata))
+    print(terazdata)
+    print()
+    print(start)
+    print()
+    print(type(start))
+    # search_created = datetime.strptime('2022-03-25 21:11:57', '%Y-%m-%d %H:%M:%S')
+    # print(type(search_created))
+
+    # print(expenses)
+    # print(expenses[1].date_created)
     total_expenses = 0
     for expense in expenses:
         total_expenses = total_expenses + expense.amount
     if request.method == "POST":
         if form.validate_on_submit():
             new_expense = Expense(name = form.name.data, amount = form.amount.data, label = form.labell.data, user_id = current_user.id)
-            # datah = request.form.get("datah")
+            datah = request.form.get("datah")
             # datah = datetime.now()
-            # print(datah)
+            print(datah[5:10])
             db.session.add(new_expense)
             db.session.commit()
             return redirect(url_for('views.home'))
